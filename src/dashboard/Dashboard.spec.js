fdfb13f90d43e3aee604f71d4e35b7650c1ad2a9
McNultyPT <mcnultypt@gmail.com>
2019-03-13 12:50:18 -0700

    writing test to check if Dashboard component renders without crashing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// initial push
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Dashboard from './Dashboard.js';
+
+describe('<Dashboard />', () => {
+    it('renders without crashing', () => {
+        render(<Dashboard />)
+    });
+});
